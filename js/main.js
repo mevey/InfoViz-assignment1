acb19d9a8eea1c848230a0b45c84af40c611eee5
mevey <muthoni90@gmail.com>
2018-02-10 20:25:45 -0800

    clean code

--- js/main.js
+++ js/main.js
@@ -46,14 +46,14 @@ DATA = [
         "operational_errors_data": [50,50,50,50],
         "mortality_rate_data": [15,10,10,4],
         "max":1000,
-        "color": "red"
+        "color": "#888888"
     },
     {
         "title": "IV Errors",
         "operational_errors_data": [50,50,50,50],
         "mortality_rate_data": [10,10,5,5],
         "max":1000,
-        "color": "red"
+        "color": "#888888"
     },
 ]
 
@@ -192,106 +192,3 @@ var chart = $('#mini-chart-6').highcharts();
 chart.series[0].data[2].update({color:'red'});
 var chart = $('#mini-chart-5').highcharts();
 chart.series[1].data[3].update({color:'red'});
-
-
-//Design 2
-//categories = ["2012", "2013", "2014", "2015"]
-//barchart_data = []
-//for(i in DATA) {
-//    barchart_data.push({"name": DATA[i].title, "data": DATA[i].operational_errors_data})
-//}
-//barchart_data.push({"name": "Average mortality rate", "type":"spline", "data": [26.25, 16.25, 7.125, 4.5], "yAxis": 1, "color":Highcharts.getOptions().colors[7]})
-//
-//main_chart_options = {
-//    chart: {
-//        type: 'column'
-//    },
-//    title: {
-//        text: 'Operational errors between 2012-2015'
-//    },
-//
-//    xAxis: {
-//        categories: categories,
-//    },
-//    yAxis: [
-//    {
-//        min: 0,
-//        title: {
-//            text: 'Number of operational errors'
-//        }
-//    },
-//    {
-//        min: 0,
-//        title: {
-//            text: 'Average mortality rate'
-//        },
-//        opposite: true,
-//        style: {
-//            color: Highcharts.getOptions().colors[7]
-//        }
-//    }
-//    ],
-//    tooltip: {
-////        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-//        pointFormat: '',
-//        shared: true,
-//        useHTML: true
-//    },
-//    plotOptions: {
-//        column: {
-//            pointPadding: 0.2,
-//            borderWidth: 0
-//        }
-//    },
-//    series: barchart_data
-//}
-//Highcharts.chart('main-chart-2', main_chart_options);
-//
-////Design 3
-//categories = []
-//barchart_data = []
-//d_2012 = [], d_2013 = [], d_2014 = [], d_2015 = []
-//for(i in DATA) {
-//    categories.push(DATA[i].title)
-//    d_2012.push(DATA[i].operational_errors_data[0])
-//    d_2013.push(DATA[i].operational_errors_data[1])
-//    d_2014.push(DATA[i].operational_errors_data[2])
-//    d_2015.push(DATA[i].operational_errors_data[3])
-//}
-//barchart_data.push({"name": "2012", "data": d_2012})
-//barchart_data.push({"name": "2013", "data": d_2013})
-//barchart_data.push({"name": "2014", "data": d_2014})
-//barchart_data.push({"name": "2015", "data": d_2015})
-//
-//main_chart_options = {
-//    chart: {
-//        type: 'column'
-//    },
-//    title: {
-//        text: 'Operational errors between 2012-2015'
-//    },
-//
-//    xAxis: {
-//        categories: categories,
-//    },
-//    yAxis: {
-//        min: 0,
-//        title: {
-//            text: 'Number of operational errors'
-//        }
-//    },
-//    tooltip: {
-////        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-//        pointFormat: '',
-//        shared: true,
-//        useHTML: true
-//    },
-//    plotOptions: {
-//        column: {
-//            pointPadding: 0.2,
-//            borderWidth: 0
-//        }
-//    },
-//    series: barchart_data
-//}
-//Highcharts.chart('main-chart-3', main_chart_options);
