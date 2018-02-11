af076243b25e3a3ef8ce330eb4b0d3ad7d132411
mevey <muthoni90@gmail.com>
2018-02-10 20:26:56 -0800

    color

--- js/main.js
+++ js/main.js
@@ -4,56 +4,56 @@ DATA = [
         "operational_errors_data": [7200,6500,5000,3000],
         "mortality_rate_data": [45,20,10,5],
         "max":7200,
-        "color": "#888888"
+        "color": Highcharts.getOptions().colors[0]
     },
     {
         "title": "Catheter Infections",
         "operational_errors_data": [4500,4000,2400,500],
         "mortality_rate_data": [15,10,2,2],
         "max":7200,
-        "color":  "#888888"
+        "color":  Highcharts.getOptions().colors[0]
     },
     {
         "title": "Surgical Errors",
         "operational_errors_data": [3300,3200,3000,1000],
         "mortality_rate_data": [20,10,5,0],
         "max":7200,
-        "color": "#888888"
+        "color": Highcharts.getOptions().colors[0]
     },
     {
         "title": "Incorrect Dosage Errors",
         "operational_errors_data": [1100,1000,400,100],
         "mortality_rate_data": [30,20,15,5],
         "max":7200,
-        "color":  "#888888"
+        "color":  Highcharts.getOptions().colors[0]
     },
     {
         "title": "Incorrect Medication",
         "operational_errors_data": [900,750,500,400],
         "mortality_rate_data": [20,10,5,10],
         "max":7200,
-        "color": "#888888"
+        "color":Highcharts.getOptions().colors[0]
     },
     {
         "title": "Misdiagnosis",
         "operational_errors_data": [1000,900,950,100],
         "mortality_rate_data": [55,40,5,5],
         "max":7200,
-        "color": "#888888"
+        "color": Highcharts.getOptions().colors[0]
     },
     {
         "title": "Delays in Treatment",
         "operational_errors_data": [50,50,50,50],
         "mortality_rate_data": [15,10,10,4],
         "max":1000,
-        "color": "#888888"
+        "color": Highcharts.getOptions().colors[0]
     },
     {
         "title": "IV Errors",
         "operational_errors_data": [50,50,50,50],
         "mortality_rate_data": [10,10,5,5],
         "max":1000,
-        "color": "#888888"
+        "color": Highcharts.getOptions().colors[0]
     },
 ]
 
